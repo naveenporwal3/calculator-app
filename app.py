6ad4782d1c9b14c857b46bcbfc45776e786e7b47
Naveen K porwal <61084852+naveenporwal3@users.noreply.github.com>
2025-09-01 22:46:32 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+st.title("Simple Calculator ")
+
+a = st.number_input("Enter your first number:")
+b = st.number_input("Enter your second number:")
+operator = st.selectbox("Choose an operator:", ["+", "-", "*", "/"])
+
+def calculator(a, b, operator):
+    if operator == "+":
+        return a + b
+    elif operator == "-":
+        return a - b
+    elif operator == "*":
+        return a * b
+    elif operator == "/":
+        if b == 0:
+            return "Cannot divide by zero"
+        return a / b
+    else:
+        return "Invalid operator"
+
+if st.button("Calculate"):
+    result = calculator(a, b, operator)
+    st.success(f"Result: {result}")
